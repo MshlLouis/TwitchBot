463ee47cb4798ccc535a54c079fd2c5c5a485b38
LouisFranz <louis.franz01@gmail.com>
2024-07-26 16:10:16 +0200

    Removed unused method

--- src/main/java/mySQLFile.java
+++ src/main/java/mySQLFile.java
@@ -221,28 +221,6 @@ public class mySQLFile {
         c.commit();
     }
 
-    public void selectData(Connection c, Statement stmt) throws SQLException {
-        stmt = c.createStatement();
-        ResultSet rs = stmt.executeQuery( "SELECT * FROM ohnePixel;" );
-        while ( rs.next() ) {
-            int id = rs.getInt("id");
-            String  name = rs.getString("name");
-            int age  = rs.getInt("age");
-            String  address = rs.getString("address");
-            float salary = rs.getFloat("salary");
-            System.out.println( "ID = " + id );
-            System.out.println( "NAME = " + name );
-            System.out.println( "AGE = " + age );
-            System.out.println( "ADDRESS = " + address );
-            System.out.println( "SALARY = " + salary );
-            System.out.println();
-        }
-        rs.close();
-        stmt.close();
-
-        System.out.println("Operation done successfully");
-    }
-
     public static void main( String args[] ) throws SQLException {
 
     }
